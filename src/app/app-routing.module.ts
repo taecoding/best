f0fabab28621ac24acfe9d101eb3dee6cd5f4742
tjung <taeheejung@gmail.com>
2019-07-06 16:27:26 -0700

    deploy-url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
+  { path: '/dashboard', component: DashboardComponent },
+  { path: '/page1', component: Page1Component },
+  { path: '/page2', component: Page2Component },
 ];
 
 @NgModule({
